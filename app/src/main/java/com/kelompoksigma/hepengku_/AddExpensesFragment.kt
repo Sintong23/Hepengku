b530f076303fbb36c4f0135e3d598e49e122e45c
unknown <satria.munthe513@gmail.com>
2024-12-12 20:08:49 +0700

    menambahkan tombol expense dan income

--- app/src/main/java/com/kelompoksigma/hepengku_/AddExpensesFragment.kt
+++ app/src/main/java/com/kelompoksigma/hepengku_/AddExpensesFragment.kt
@@ -5,21 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.kelompoksigma.hepengku_.databinding.FragmentAddExpensesBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddExpensesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class AddExpensesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentAddExpensesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +31,17 @@ class AddExpensesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_expenses, container, false)
+        _binding = FragmentAddExpensesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Tambahkan tombol untuk navigasi ke addIncome
+        binding.view15.setOnClickListener {
+            findNavController().navigate(R.id.addIncomeFragment) // Pastikan ID ini ada di main_nav.xml
+        }
     }
 
     companion object {
--- app/src/main/java/com/kelompoksigma/hepengku_/AddIncomeFragment.kt
+++ app/src/main/java/com/kelompoksigma/hepengku_/AddIncomeFragment.kt
@@ -5,21 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.kelompoksigma.hepengku_.databinding.FragmentAddIncomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddIncomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddIncomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentAddIncomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +30,21 @@ class AddIncomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_income, container, false)
+        _binding = FragmentAddIncomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Tambahkan tombol untuk navigasi ke addIncome
+        binding.view14.setOnClickListener {
+            findNavController().navigate(R.id.addExpensesFragment) // Pastikan ID ini ada di main_nav.xml
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddIncomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             AddIncomeFragment().apply {
